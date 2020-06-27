8bad1416ebbc2ed20d4d083a883b3cc1600ba967
Yeondam Park <lloyd.park88@gmail.com>
2020-06-26 19:56:07 -0700

    setting up react-native with expo

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { AppLoading } from 'expo';
+import { Text, Image } from 'react-native';
+import { Asset } from 'expo-asset';
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    console.log(image);
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => {
+    setIsReady(true);
+  };
+  const loadAssets = async () => {
+    const images = [
+      require('./assets/login-bg.jpeg'),
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngguru.com%2Ffree-transparent-background-png-clipart-mucfk&psig=AOvVaw3YR_0ySMfG_GwU-iX0_FSz&ust=1593312292636000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiNx_38oOoCFQAAAAAdAAAAABAD',
+    ];
+
+    cacheImages(images);
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      handleFinish={handleFinish}
+      startAsync={loadAssets}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
