5764055667686c919011e6acedf864025e3c00cf
Yeondam Park <lloyd.park88@gmail.com>
2020-07-16 21:19:58 -0700

    add tabs navigation

--- components/Gate.js
+++ components/Gate.js
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { logIn, logOut } from '../redux/usersSlice';
 import Auth from '../navigation/Auth';
+import Main from '../navigation/Main';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default () => {
@@ -11,16 +12,7 @@ export default () => {
 
   return (
     <NavigationContainer>
-      {isLoggedIn ? (
-        <TouchableOpacity
-          onPress={() => dispatch(logOut())}
-          style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}
-        >
-          <Text>Log Out</Text>
-        </TouchableOpacity>
-      ) : (
-        <Auth />
-      )}
+      {isLoggedIn ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 };
