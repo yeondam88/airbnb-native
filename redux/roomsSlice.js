dd996c3d74691fd463e3c40e90b556c36b1dda64
Yeondam Park <lloyd.park88@gmail.com>
2020-07-23 23:32:13 -0700

    fixing bugs on load more

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -14,14 +14,19 @@ const roomsSlice = createSlice({
     setExploreRooms(state, action) {
       const { explore } = state;
       const { payload } = action;
-      payload.rooms.forEach((payloadRoom) => {
-        const exists = explore.rooms.find(
-          (savedRoom) => savedRoom.id === payloadRoom.id
-        );
-        if (!exists) {
-          explore.rooms.push(payloadRoom);
-        }
-      });
+      if (payload.page === 1) {
+        state.explore.rooms = payload.rooms;
+        state.explore.page = 1;
+      } else {
+        payload.rooms.forEach((payloadRoom) => {
+          const exists = explore.rooms.find(
+            (savedRoom) => savedRoom.id === payloadRoom.id
+          );
+          if (!exists) {
+            explore.rooms.push(payloadRoom);
+          }
+        });
+      }
     },
     increasePage(state, action) {
       state.explore.page += 1;
@@ -29,7 +34,7 @@ const roomsSlice = createSlice({
   },
 });
 
-const { setExploreRooms, increasePage } = roomsSlice.actions;
+export const { setExploreRooms, increasePage } = roomsSlice.actions;
 
 export const getRooms = (page) => async (dispatch) => {
   try {
@@ -39,6 +44,7 @@ export const getRooms = (page) => async (dispatch) => {
     dispatch(
       setExploreRooms({
         rooms: results,
+        page,
       })
     );
   } catch (error) {
--- screens/Main/Explore/ExploreContainer.js
+++ screens/Main/Explore/ExploreContainer.js
@@ -12,6 +12,10 @@ const Text = styled.Text``;
 
 export default ({ getRooms, rooms, page, increasePage }) => {
   useEffect(() => {
+    getRooms(1);
+  }, []);
+
+  useEffect(() => {
     getRooms(page);
   }, [page]);
   return <ExplorePresenter rooms={rooms} increasePage={increasePage} />;
